6facdb35cb086c3cbe264892ef12ccab1b28b959
nictrak <narin14175@gmail.com>
2020-12-17 19:25:56 +0700

    Grid render complete

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Cell : MonoBehaviour
+{
+    private string houseMap;
+    private float heat;
+    private bool levelOneFire;
+    private bool levelTwoFire;
+    private bool levelThreeFire;
+    private GameObject fireSimulation;
+
+    public Color NormalColor;
+    public Color WallColor;
+    public string HouseMap { get => houseMap; set => houseMap = value; }
+    public float Heat { get => heat; set => heat = value; }
+    public bool LevelOneFire { get => levelOneFire; set => levelOneFire = value; }
+    public bool LevelTwoFire { get => levelTwoFire; set => levelTwoFire = value; }
+    public bool LevelThreeFire { get => levelThreeFire; set => levelThreeFire = value; }
+    public GameObject FireSimulation { get => fireSimulation; set => fireSimulation = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        houseMap = "Normal";
+        heat = 0;
+        levelOneFire = false;
+        levelTwoFire = false;
+        levelThreeFire = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GridSystem : MonoBehaviour
+{
+    private List<List<Cell>> grids;
+    public Vector3 OriginPosition;
+    public Vector2 CellSize;
+    public Vector2 GridSize;
+    public Cell GridPrefab;
+    public float BaseZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grids = new List<List<Cell>>();
+        for (int j = 0; j < GridSize.y; j++)
+        {
+            List<Cell> newRow = new List<Cell>();
+            for (int i = 0; i < GridSize.x; i++)
+            {
+                float xPosition = OriginPosition.x + i * CellSize.x;
+                float yPosition = OriginPosition.y + -j * CellSize.y;
+                Vector3 instatiatePosition = new Vector3(xPosition, yPosition, BaseZ);
+                Cell newGrid = Instantiate<Cell>(GridPrefab);
+                newGrid.transform.position = instatiatePosition;
+                newRow.Add(newGrid);
+            }
+            grids.Add(newRow);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
