8dcd3a2dfcf1bd4dd811f7346b51217200d76e0d
nictrak <narin14175@gmail.com>
2020-12-18 16:51:18 +0700

    Fuck off numpy.net

---
+++ Assets/Scripts/GrabHitbox.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class GrabHitbox : MonoBehaviour
+{
+    private List<Grabbable> grabbables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Grabbable grabbable = collision.gameObject.GetComponent<Grabbable>();
+        if (grabbable != null)
+        {
+            grabbables.Add(grabbable);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Grabbable grabbable = collision.gameObject.GetComponent<Grabbable>();
+        if (grabbable != null)
+        {
+            grabbables.Remove(grabbable);
+        }
+    }
+    private Grabbable CalNearest()
+    {
+        if (grabbables.Count == 0)
+        {
+            return null;
+        }
+        float shortestDistance = float.MaxValue;
+        Grabbable temp;
+        temp = grabbables[0];
+        for (int i = 0; i < grabbables.Count; i++)
+        {
+            temp = grabbables[i];
+            
+        }
+        return temp;
+    }
+}
--- Assets/Scripts/Life.cs
+++ Assets/Scripts/Life.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Numpy;
+using NumSharp;
 
 public class Life : MonoBehaviour
 {
     public float lifePoint;
 
-
     // Start is called before the first frame update
     void Start()
     {
-
+        var nd = np.full(5, 12);
+        Debug.Log(nd);
     }
 
     // Update is called once per frame
