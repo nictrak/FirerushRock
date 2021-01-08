8b88ae0c90826f554f3348b8229ffc531a89e55e
phumnoi1 <phumnoi1@gmail.com>
2021-01-08 23:20:42 +0700

    add default song

--- Assets/MenuScript/MainMenu.cs
+++ Assets/MenuScript/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioClip DefaultMusic;
+    public void Start()
+    {
+        GameConfig.Music = DefaultMusic;
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
