0cb1b0ee3fb3377811b55d91fcf916f4ee052151
nictrak <narin14175@gmail.com>
2021-01-08 20:38:58 +0700

    Revert "Merge branch 'master' into Menu"
    This reverts commit e95a508b12058b4856ee5b592c079dd55d37a54b, reversing
    changes made to 433473d1613d675894b2261f10d8aa829c757b95.

---
+++ Assets/MenuScript/IntroDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDelay : MonoBehaviour
+{
+
+    public int Countdown;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count >= Countdown)
+        {
+            this.gameObject.SetActive(false);
+        }
+        count += 1;
+    }
+}
