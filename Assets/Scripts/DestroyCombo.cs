a611d4d10e34a2498db5b206ada74177b8300663
nictrak <narin14175@gmail.com>
2021-01-18 21:12:41 +0700

    Fix prefab

---
+++ Assets/Scripts/DestroyCombo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCombo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(transform.childCount > 0)
+        {
+            transform.GetChild(0).parent = null;
+        }
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
