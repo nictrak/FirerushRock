930db3cbd011ae87c9eae0b7cc262a7b326456c7
nictrak <narin14175@gmail.com>
2021-01-19 03:38:19 +0700

    Toggle ready complete

---
+++ Assets/Scripts/MenuScript/Ready.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class Ready : NetworkBehaviour
+{
+    public Text text;
+    private bool isReady;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isReady = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToggleReady()
+    {
+        isReady = !isReady;
+        if (isReady) text.text = "Not Ready";
+        else text.text = "Ready";
+    }
+}
