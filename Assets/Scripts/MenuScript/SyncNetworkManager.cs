da0fbf02c3581aeb344956ff58b315545cc0de57
nictrak <narin14175@gmail.com>
2021-01-20 03:04:03 +0700

    Fix server bug

---
+++ Assets/Scripts/MenuScript/SyncNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class SyncNetworkManager : MonoBehaviour
+{
+    private NetworkManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("Network").GetComponent<NetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartHost()
+    {
+        manager.StartHost();
+    }
+    public void StartClient()
+    {
+        manager.StartClient();
+    }
+}
