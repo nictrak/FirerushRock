f2890ee5faeb17bdf59a0b5e056266fedbf6cefb
phumnoi1 <phumnoi1@gmail.com>
2021-01-20 20:57:44 +0700

    adjust menu intro light

--- Assets/Scripts/MenuScript/IntroPanel.cs
+++ Assets/Scripts/MenuScript/IntroPanel.cs
@@ -8,6 +8,7 @@ public class IntroPanel : MonoBehaviour
     // Start is called before the first frame update
     public int IntroFrame;
     public float ColorChangeRate;
+    public float FadeRate;
     private int count;
     private Image img;
     private int Phase;
@@ -20,7 +21,7 @@ public class IntroPanel : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(count >= IntroFrame & Phase == 1)
         {
@@ -33,7 +34,7 @@ public class IntroPanel : MonoBehaviour
         }
         if (Phase == 2)
         {
-            img.color = new Color(1, 1, 1, img.color[3] - (float) 0.001);
+            img.color = new Color(1, 1, 1, img.color[3] - FadeRate);
             if (img.color[3] <= 0)
             {
                 Destroy(this.gameObject);
