055345fbaddc9bbb66156c0d9bcadad01d2fdd4d
phumnoi1 <phumnoi1@gmail.com>
2021-01-20 23:33:14 +0700

    Lobby complete!!

---
+++ Assets/LobbyMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class LobbyMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource music;
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+        music.volume = GameConfig.MusicVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
