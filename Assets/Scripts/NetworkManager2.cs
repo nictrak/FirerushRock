e94c6e7687b338db661b90bcc0d196dea1898eb4
nictrak <narin14175@gmail.com>
2021-01-22 18:44:05 +0700

    Network2 work properly like network1

--- Assets/Scripts/NetworkManager2.cs
+++ Assets/Scripts/NetworkManager2.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NetworkManager2 : MonoBehaviour
+using Mirror;
+public class NetworkManager2 : NetworkManager
 {
     // Start is called before the first frame update
     void Start()
