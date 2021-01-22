bdb1f51dd52ee5676ede7a787b9e5127d1f56966
nictrak <narin14175@gmail.com>
2021-01-22 22:24:42 +0700

    Network Manager 2 complete

--- Assets/Scripts/NetworkManager2.cs
+++ Assets/Scripts/NetworkManager2.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 using Mirror;
 public class NetworkManager2 : NetworkManager
 {
+    public GameObject PlayerPrefab1;
+    public GameObject PlayerPrefab2;
+    public GameObject PlayerPrefab3;
+    public GameObject PlayerPrefab4;
+
+    private bool isUse1;
+    private bool isUse2;
+    private bool isUse3;
+    private bool isUse4;
+
+    private int player1;
+    private int player2;
+    private int player3;
+    private int player4;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,69 @@ public class NetworkManager2 : NetworkManager
     void Update()
     {
         
+    }
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        GameObject gameobject;
+        gameobject = SpawnOnStart(SelectPrefab(conn));
+        NetworkServer.AddPlayerForConnection(conn, gameobject);
+    }
+    private GameObject SpawnOnStart(GameObject prefab)
+    {
+        Transform startPos = GetStartPosition();
+        GameObject obj = startPos != null
+                ? Instantiate(prefab, startPos.position, startPos.rotation)
+                : Instantiate(prefab);
+        return obj;
+    }
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (conn.connectionId == player1)
+        {
+            isUse1 = false;
+        }
+        else if (conn.connectionId == player2)
+        {
+            isUse2 = false;
+        }
+        else if (conn.connectionId == player3)
+        {
+            isUse3 = false;
+        }
+        else if (conn.connectionId == player4)
+        {
+            isUse4 = false;
+        }
+        base.OnServerDisconnect(conn);
+    }
+    private GameObject SelectPrefab(NetworkConnection conn)
+    {
+        GameObject obj = null;
+        if (!isUse1)
+        {
+            obj = PlayerPrefab1;
+            isUse1 = true;
+            player1 = conn.connectionId;
+        }
+        else if (!isUse2)
+        {
+            obj = PlayerPrefab2;
+            isUse2 = true;
+            player2 = conn.connectionId;
+        }
+        else if (!isUse3)
+        {
+            obj = PlayerPrefab3;
+            isUse3 = true;
+            player3 = conn.connectionId;
+        }
+        else if (!isUse4)
+        {
+            obj = PlayerPrefab4;
+            isUse4 = true;
+            player4 = conn.connectionId;
+        }
+        else obj = PlayerPrefab1;
+        return obj;
     }
 }
