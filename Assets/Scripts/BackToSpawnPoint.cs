6ccda88c8877b6fc3e14cdef62482ec32029b7a2
nictrak <narin14175@gmail.com>
2021-01-23 21:16:38 +0700

    Add back to spawn point and Delay player move time

---
+++ Assets/Scripts/BackToSpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToSpawnPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Firefighter player = collision.gameObject.GetComponent<Firefighter>();
+        if(player != null)
+        {
+            player.ToSpawnPoint();
+        }
+    }
+}
