6bf9a23cfcb5bb9bf21095c5819ec000baee9489
phumnoi1 <phumnoi1@gmail.com>
2021-02-21 20:17:54 +0700

    chage all fonts, back button will be red when hover

---
+++ Assets/Scripts/UI/ReturnLobbyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReturnLobbyText : MonoBehaviour
+{
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void ChangeToRed()
+    {
+        text.color = Color.red;
+    }
+
+    public void ChangeToWhite()
+    {
+        text.color = Color.white;
+    }
+}
